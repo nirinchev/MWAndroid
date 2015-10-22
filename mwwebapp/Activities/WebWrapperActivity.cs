88dfb880473db50bc1cc36b62de6338c29c1777d
Nikola Irinchev <irinchev@me.com>
2015-10-23 00:29:42 +0300

    Switch to fragment

--- mwwebapp/Activities/WebWrapperActivity.cs
+++ mwwebapp/Activities/WebWrapperActivity.cs
@@ -1,6 +1,5 @@
 ﻿using Android.App;
 using Android.OS;
-using Android.Webkit;
 using Android.Views;
 
 namespace MWWebApp
@@ -9,66 +8,32 @@ namespace MWWebApp
 	public class WebWrapperActivity : Activity
 	{
 		public const string WebAddressKey = "WebAddress";
-		private readonly MWWebViewClient webViewClient = new MWWebViewClient();
 
-		private WebView webView;
+		private WebWrapperFragment webFragment;
 
 		protected override void OnCreate(Bundle bundle)
 		{
 			base.OnCreate(bundle);
 
 			this.RequestWindowFeature(WindowFeatures.NoTitle);
+			this.SetContentView(Resource.Layout.WebWrapperActivity);
 
-			this.SetContentView(Resource.Layout.WebWrapper);
-
-			this.webView = this.FindViewById<WebView>(Resource.Id.webView);
-			this.webView.Settings.LoadWithOverviewMode = true;
-			this.webView.Settings.BuiltInZoomControls = true;
-			this.webView.Settings.DisplayZoomControls = false;
-			this.webView.Settings.UseWideViewPort = true;
-			this.webView.Settings.JavaScriptEnabled = true;
-			this.webView.Settings.JavaScriptCanOpenWindowsAutomatically = true;
-			this.webView.SetInitialScale(0);
-			this.webView.LoadUrl(this.Intent.GetStringExtra(WebAddressKey));
-			this.webView.SetWebViewClient(this.webViewClient);
-		}
-
-		protected override void OnSaveInstanceState(Bundle outState)
-		{
-			base.OnSaveInstanceState(outState);
-
-			outState.PutString(WebAddressKey, this.webView.Url);
-		}
-
-		protected override void OnRestoreInstanceState(Bundle savedInstanceState)
-		{
-			base.OnRestoreInstanceState(savedInstanceState);
-
-			var url = savedInstanceState.GetString(WebAddressKey);
-			if (!string.IsNullOrEmpty(url))
+			this.webFragment = this.FragmentManager.FindFragmentById<WebWrapperFragment>(Resource.Id.webwrapperfragment);
+			if (string.IsNullOrEmpty(this.webFragment.Url))
 			{
-				this.webView.LoadUrl(url);
+				this.webFragment.Url = this.Intent.GetStringExtra(WebAddressKey);
 			}
 		}
 
 		public override void OnBackPressed()
 		{
-			if (this.webView.CanGoBack())
+			if (this.webFragment.CanGoBack)
 			{
-				this.webView.GoBack();
+					this.webFragment.GoBack();
 			}
 			else
 			{
 				base.OnBackPressed();
-			}
-		}
-
-		private class MWWebViewClient : WebViewClient
-		{
-			public override bool ShouldOverrideUrlLoading(WebView view, string url)
-			{
-				view.LoadUrl(url);
-				return true;
 			}
 		}
 	}
---
+++ mwwebapp/Activities/WebWrapperFragment.cs
@@ -0,0 +1,95 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Views;
+using Android.Webkit;
+using Android.Content;
+using Android.Net;
+using Android.Runtime;
+using Android.Widget;
+
+namespace MWWebApp
+{
+	public class WebWrapperFragment : Fragment
+	{
+		private Bundle webViewState;
+		private WebView webView;
+
+		public string Url 
+		{
+			get
+			{
+				return this.webView.Url;
+			}
+			set
+			{
+				if (value != this.webView.Url)
+				{
+					this.webView.LoadUrl(value);
+				}
+			}
+		}
+
+		public bool CanGoBack
+		{
+			get
+			{
+				return this.webView.CanGoBack();
+			}
+		}
+
+		public override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			this.RetainInstance = true;
+		}
+
+		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			var view =  inflater.Inflate(Resource.Layout.WebWrapper, container, false);
+			this.webView = view.FindViewById<WebView>(Resource.Id.webView);
+			this.webView.Settings.LoadWithOverviewMode = true;
+			this.webView.Settings.BuiltInZoomControls = true;
+			this.webView.Settings.DisplayZoomControls = false;
+			this.webView.Settings.UseWideViewPort = true;
+			this.webView.Settings.JavaScriptEnabled = true;
+			this.webView.Settings.JavaScriptCanOpenWindowsAutomatically = true;
+			this.webView.SetInitialScale(0);
+			this.webView.SetWebViewClient(new WebViewClient());
+			this.webView.Download += this.OnFileDownload;
+			if (this.webViewState != null)
+			{
+				this.webView.RestoreState(this.webViewState);
+			}
+
+			return view;
+		}
+
+		private void OnFileDownload (object sender, DownloadEventArgs e)
+		{
+			var request = new DownloadManager.Request(Uri.Parse(e.Url));
+			// TODO: this should be fixed on the server!
+			request.AllowScanningByMediaScanner();
+			request.SetNotificationVisibility(DownloadVisibility.VisibleNotifyCompleted);
+			// TODO: set proper file name
+			//e.u
+			request.SetDestinationInExternalPublicDir(Environment.DirectoryDownloads, "report.docx");
+			var dm = this.Activity.GetSystemService(Context.DownloadService).JavaCast<DownloadManager>();
+			dm.Enqueue(request);
+			this.GoBack();
+			Toast.MakeText(this.Activity, "Свалям файла...", ToastLength.Long).Show();
+		}
+
+		public override void OnPause()
+		{
+			this.webViewState = new Bundle();
+			this.webView.SaveState(this.webViewState);
+			this.webView.Download -= this.OnFileDownload;
+			base.OnPause();
+		}
+
+		public void GoBack()
+		{
+			this.webView.GoBack();
+		}
+	}
+}
