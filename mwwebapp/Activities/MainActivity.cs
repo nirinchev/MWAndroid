3da4b6b886f78fe743897f3407861fd285a9b501
Nikola Irinchev <irinchev@me.com>
2015-10-22 21:38:26 +0300

    Initial commit

---
+++ mwwebapp/Activities/MainActivity.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Refractored.Xam.Settings;
+using Android.Content;
+
+namespace MWWebApp
+{
+	[Activity(Label = "MWWebApp", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		private const string WebUrlKey = "WebUrl";
+
+		private Button startButton;
+		private EditText urlEditText;
+		private CheckBox rememberUrlCheckBox;
+
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			this.SetContentView(Resource.Layout.Main);
+
+			this.urlEditText = FindViewById<EditText>(Resource.Id.txtUrl);
+			this.rememberUrlCheckBox = FindViewById<CheckBox>(Resource.Id.cbxRememberUrl);
+			this.startButton = FindViewById<Button>(Resource.Id.btnStart);
+
+			var savedUrl = CrossSettings.Current.GetValueOrDefault<string>(WebUrlKey);
+			if (!string.IsNullOrEmpty(savedUrl))
+			{
+				this.urlEditText.Text = savedUrl;
+				this.rememberUrlCheckBox.Checked = true;
+
+				this.ShowWebVersion(savedUrl);
+			}
+		}
+
+		protected override void OnStart()
+		{
+			base.OnStart();
+			this.startButton.Click += this.OnStartButtonClicked;
+		}
+
+		protected override void OnStop()
+		{
+			base.OnStop();
+			this.startButton.Click -= this.OnStartButtonClicked;
+		}
+
+		private void OnStartButtonClicked(object sender, EventArgs e)
+		{
+			var url = this.urlEditText.Text;
+			if (!string.IsNullOrEmpty(url))
+			{
+				if (this.rememberUrlCheckBox.Checked)
+				{
+					CrossSettings.Current.AddOrUpdateValue(WebUrlKey, url);
+				}
+				else
+				{
+					CrossSettings.Current.AddOrUpdateValue(WebUrlKey, string.Empty);
+				}
+
+				this.ShowWebVersion(url);
+			}
+		}
+
+		private void ShowWebVersion(string url)
+		{
+			var intent = new Intent(this, typeof(WebWrapperActivity));
+			intent.PutExtra(WebWrapperActivity.WebAddressKey, url);
+			this.StartActivity(intent);
+		}
+	}
+}
---
+++ mwwebapp/Activities/WebWrapperActivity.cs
@@ -0,0 +1,75 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Webkit;
+using Android.Views;
+
+namespace MWWebApp
+{
+	[Activity(Label = "WebWrapperActivity")]			
+	public class WebWrapperActivity : Activity
+	{
+		public const string WebAddressKey = "WebAddress";
+		private readonly MWWebViewClient webViewClient = new MWWebViewClient();
+
+		private WebView webView;
+
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			this.RequestWindowFeature(WindowFeatures.NoTitle);
+
+			this.SetContentView(Resource.Layout.WebWrapper);
+
+			this.webView = this.FindViewById<WebView>(Resource.Id.webView);
+			this.webView.Settings.LoadWithOverviewMode = true;
+			this.webView.Settings.BuiltInZoomControls = true;
+			this.webView.Settings.DisplayZoomControls = false;
+			this.webView.Settings.UseWideViewPort = true;
+			this.webView.Settings.JavaScriptEnabled = true;
+			this.webView.Settings.JavaScriptCanOpenWindowsAutomatically = true;
+			this.webView.SetInitialScale(0);
+			this.webView.LoadUrl(this.Intent.GetStringExtra(WebAddressKey));
+			this.webView.SetWebViewClient(this.webViewClient);
+		}
+
+		protected override void OnSaveInstanceState(Bundle outState)
+		{
+			base.OnSaveInstanceState(outState);
+
+			outState.PutString(WebAddressKey, this.webView.Url);
+		}
+
+		protected override void OnRestoreInstanceState(Bundle savedInstanceState)
+		{
+			base.OnRestoreInstanceState(savedInstanceState);
+
+			var url = savedInstanceState.GetString(WebAddressKey);
+			if (!string.IsNullOrEmpty(url))
+			{
+				this.webView.LoadUrl(url);
+			}
+		}
+
+		public override void OnBackPressed()
+		{
+			if (this.webView.CanGoBack())
+			{
+				this.webView.GoBack();
+			}
+			else
+			{
+				base.OnBackPressed();
+			}
+		}
+
+		private class MWWebViewClient : WebViewClient
+		{
+			public override bool ShouldOverrideUrlLoading(WebView view, string url)
+			{
+				view.LoadUrl(url);
+				return true;
+			}
+		}
+	}
+}
